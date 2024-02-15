46f06a8aecf831112edd6f963c38c6517ce9ffea
Kailen41 <csilveira81806@gmail.com>
2024-02-15 04:55:19 -0500

    Player control movement and animations

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -6,9 +6,13 @@ public class PlayerManager : MonoBehaviour
 {
     [SerializeField] Rigidbody2D _playerRB;
 
+    [SerializeField] Animator _playerAnimator;
+
+    private float _playerSpeed = 1000f;
+
     void Start()
     {
-        
+
     }
 
     void Update()
@@ -16,6 +20,15 @@ public class PlayerManager : MonoBehaviour
         float _horizontalMovement = Input.GetAxisRaw("Horizontal");
         float _verticalMovement = Input.GetAxisRaw("Vertical");
 
-        _playerRB.velocity = new Vector2(_horizontalMovement, _verticalMovement);
+        _playerRB.velocity = new Vector2(_horizontalMovement, _verticalMovement) * Time.deltaTime * _playerSpeed;
+
+        _playerAnimator.SetFloat("MovementX", _playerRB.velocity.x);
+        _playerAnimator.SetFloat("MovementY", _playerRB.velocity.y);
+
+        if (_horizontalMovement == 1 || _horizontalMovement == -1 || _verticalMovement == 1 || _verticalMovement == -1)
+        {
+            _playerAnimator.SetFloat("LastX", _horizontalMovement);
+            _playerAnimator.SetFloat("LastY", _verticalMovement);
+        }
     }
 }
